56ee39f3b718110348047047aed036aaf39db853
Daniel Ström <D@nielstrom.se>
2013-11-29 12:01:53 +0100

    Make sure the lib folder is on the ruby path.

--- lib/pong.rb
+++ lib/pong.rb
@@ -1,3 +1,7 @@
+unless $LOAD_PATH.include?(File.expand_path(File.dirname(__FILE__)))
+  $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__)))
+end
+
 # Provides the namespace "Pong" and starts the application when run with
 # the command 'ruby pong.rb'.
 module Pong
