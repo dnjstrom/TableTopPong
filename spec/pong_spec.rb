e767660caf8eccbcac1f91e6c7f107081f208db4
Daniel Ström <D@nielstrom.se>
2013-11-29 11:48:14 +0100

    Test for vector library inclusion.

--- spec/pong_spec.rb
+++ spec/pong_spec.rb
@@ -1,7 +1,13 @@
 require 'pong'
+require 'vector'
 
 describe Pong do
   it "is not a bartender" do
   	expect { Pong.mix(:mojito) }.to raise_error
+  end
+
+  it "loads the vector library" do
+  	v = Vector.new(3, 4)
+  	v.length.should eq 5
   end
 end
