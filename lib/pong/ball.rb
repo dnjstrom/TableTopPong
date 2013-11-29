c013ec2a597483da2f8b98d3f5726b6b3c71b3a8
Isak Stravik <istravik@gmail.com>
2013-11-29 09:06:23 +0100

    Remove indentation in class files.

--- lib/pong/ball.rb
+++ lib/pong/ball.rb
@@ -1,21 +1,21 @@
 module Pong
-	class Ball
-		attr_accessor :x, :y, :radius, :dx, :dy
+class Ball
+	attr_accessor :x, :y, :radius, :dx, :dy
 
-		def initialize(x, y, radius, dx, dy)
-			setPosition(x, y);
-			@radius = radius
-			@dx = dx
-			@dy = dy
-		end
-
-		def setPosition(x, y)
-			@x = x;
-			@y = y;
-		end
-
-		def move
-			setPosition(x + dx, y + dy)
-		end
+	def initialize(x, y, radius, dx, dy)
+		setPosition(x, y);
+		@radius = radius
+		@dx = dx
+		@dy = dy
 	end
+
+	def setPosition(x, y)
+		@x = x;
+		@y = y;
+	end
+
+	def move
+		setPosition(x + dx, y + dy)
+	end
+end
 end
--- lib/pong/paddle.rb
+++ lib/pong/paddle.rb
@@ -1,20 +1,20 @@
 module Pong
-	class Paddle
-		attr_accessor :x, :y, :rotation, :score
+class Paddle
+	attr_accessor :x, :y, :rotation, :score
 
-		def initialize(x, y, rotation)
-			@x = x
-			@y = y
-			@rotation = rotation
-		end
-
-		def increaseScore
-			@score += 1
-		end
-
-		def setPosition(x, y)
-			@x = x;
-			@y = y;
-		end
+	def initialize(x, y, rotation)
+		@x = x
+		@y = y
+		@rotation = rotation
 	end
+
+	def increaseScore
+		@score += 1
+	end
+
+	def setPosition(x, y)
+		@x = x;
+		@y = y;
+	end
+end
 end
