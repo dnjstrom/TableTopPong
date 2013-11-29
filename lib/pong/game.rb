bca2eb362d069c673df8aec8bf8d44cdac38c1ea
Isak Stravik <istravik@gmail.com>
2013-11-29 09:29:26 +0100

    Add basic game loop.

--- lib/pong/game.rb
+++ lib/pong/game.rb
@@ -6,10 +6,25 @@ class Game
 	WIDTH = 1000
 	HEIGHT = 800
 
+	tracker = Tracker.new
 
 	paddle1 = Paddle.new(20, HEIGHT / 2, 0)
 	paddle2 = Paddle.new(WIDTH - 20, HEIGHT / 2, 0)
 
 	ball = Ball.new(WIDTH / 2, HEIGHT / 2, 20, 5, 5)
+
+	# Start tracker and identify blocks?
+
+	begin # Game loop
+		# Check input
+
+		# Move paddles
+		# Move ball
+
+		# Check collisions
+		# Check score
+
+		# Draw GUI
+	end until true
 end
 end
