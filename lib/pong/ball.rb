5cec33928863346ffaffbf04bea27a6cc81ab99a
Isak Stravik <istravik@gmail.com>
2013-11-29 10:48:53 +0100

    Update the game loop with some position constraints. Add invert methods to Ball.

--- lib/pong/ball.rb
+++ lib/pong/ball.rb
@@ -8,20 +8,28 @@ class Ball
 		setDirection(dx, dy)
 	end
 
-	def setPosition(x, y)
-		@x = x
-		@y = y
-	end
-
 	def setDirection(dx, dy)
 		@dx = dx
 		@dy = dy
+	end
+
+	def invertYDirection()
+		@dy *= -1
+	end
+
+	def invertXDirection()
+		@dx *= -1
 	end
 
 	def move
 		setPosition(x + dx, y + dy)
 	end
 
-	private :setPosition
+	def setPosition(x, y)
+		@x = x
+		@y = y
+	end
+
+	private :setPosition, :setDirection
 end
 end
--- lib/pong/game.rb
+++ lib/pong/game.rb
@@ -5,6 +5,8 @@ module Pong
 class Game
 	WIDTH = 1000
 	HEIGHT = 800
+	HOME_WIDTH = 100
+	HOME_HEIGHT = HEIGHT
 
 	tracker = Tracker.new
 
@@ -13,18 +15,68 @@ class Game
 
 	ball = Ball.new(WIDTH / 2, HEIGHT / 2, 20, 5, 5)
 
-	# Start tracker and identify blocks?
+	# Start tracker and identify blocks
+	# Set identifier to paddle objects
 
 	begin # Game loop
-		# Check input
+		# Check input, use tracker
+		paddle1_new_position = 0
+		paddle2_new_position = 0
+
+		# Assure that the paddles are inside the home
+		insideHome(paddle1_new_position, paddle2_new_position);
 
 		# Move paddles
+		paddle1.setPosition(paddle1_new_position.getX, paddle1_new_position.getY)
+		paddle2.setPosition(paddle2_new_position.getX, paddle1_new_position.getY)
+
 		# Move ball
+		ball.move
 
 		# Check collisions
+
+		# Change ball direction
+		# Ball hits top or bottom wall
+		if ball.y < 0 || ball.y > HEIGHT
+			ball.invertYDirection
+		end
+
 		# Check score
+		if ball.x < 0
+			player1.increaseScore
+		elsif ball.x > WIDTH
+			player2.increaseScore
+		end
 
 		# Draw GUI
 	end until true
+
+	def insideHome(paddle1_new_position, paddle2_new_position)
+		if paddle1_new_position.getX > HOME_WIDTH
+			paddle1_new_position.x = HOME_WIDTH
+		elsif paddle1_new_position.getX < 0
+			paddle1_new_position.x = 0
+		end
+
+		if paddle1_new_position.getY > HEIGHT
+			paddle1_new_position.y = HEIGHT
+		elsif paddle1_new_position.getY < 0
+			paddle1_new_position.y = 0
+		end
+
+		if paddle2_new_position.getX < WIDTH - HOME_WIDTH
+			paddle2_new_position.x = WIDTH - HOME_WIDTH
+		elsif paddle2_new_position.getX > WIDTH
+			paddle2_new_position.x = WIDTH
+		end
+
+		if paddle2_new_position.getY > HEIGHT
+			paddle2_new_position.y = HEIGHT
+		elsif paddle2_new_position.getY < 0
+			paddle2_new_position.y = 0
+		end
+	end
+
+	private :insideHome
 end
 end
