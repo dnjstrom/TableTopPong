1437a85682c9b04e81a113e954af9ac4075097df
Isak Stravik <istravik@gmail.com>
2013-11-29 09:32:36 +0100

    Add setDirection method to Ball. Made the setPosition method private.

--- lib/pong/ball.rb
+++ lib/pong/ball.rb
@@ -3,19 +3,25 @@ class Ball
 	attr_accessor :x, :y, :radius, :dx, :dy
 
 	def initialize(x, y, radius, dx, dy)
-		setPosition(x, y);
+		setPosition(x, y)
 		@radius = radius
-		@dx = dx
-		@dy = dy
+		setDirection(dx, dy)
 	end
 
 	def setPosition(x, y)
-		@x = x;
-		@y = y;
+		@x = x
+		@y = y
+	end
+
+	def setDirection(dx, dy)
+		@dx = dx
+		@dy = dy
 	end
 
 	def move
 		setPosition(x + dx, y + dy)
 	end
+
+	private :setPosition
 end
 end
