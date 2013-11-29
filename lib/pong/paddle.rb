92631860a9f8ce81853ddda132e6472df35b03ad
Isak Stravik <istravik@gmail.com>
2013-11-29 08:48:44 +0100

    Add a basic Paddle class.

---
+++ lib/pong/paddle.rb
@@ -0,0 +1,18 @@
+class Paddle
+	attr_accessor :x, :y, :rotation, :score
+
+	def initialize(x, y, rotation)
+		@x = x
+		@y = y
+		@rotation = rotation
+	end
+
+	def increaseScore
+		@score += 1
+	end
+
+	def setPosition(x, y)
+		@x = x;
+		@y = y;
+	end
+end
