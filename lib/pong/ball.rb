2ebbc2716b176a63367125d3ecd290dc93d1e448
Isak Stravik <istravik@gmail.com>
2013-11-29 08:53:14 +0100

    Add a basic Ball class.

---
+++ lib/pong/ball.rb
@@ -0,0 +1,19 @@
+class Ball
+	attr_accessor :x, :y, :radius, :dx, :dy
+
+	def initialize(x, y, radius, dx, dy)
+		setPosition(x, y);
+		@radius = radius
+		@dx = dx
+		@dy = dy
+	end
+
+	def setPosition(x, y)
+		@x = x;
+		@y = y;
+	end
+
+	def move
+		setPosition(x + dx, y + dy)
+	end
+end
