43063b7e15df3646de96b55dc4c5c565ad566ad6
Isak Stravik <istravik@gmail.com>
2013-11-29 09:34:49 +0100

    Minor changes in Paddle. Removed semicolons.

--- lib/pong/paddle.rb
+++ lib/pong/paddle.rb
@@ -3,9 +3,9 @@ class Paddle
 	attr_accessor :x, :y, :rotation, :score
 
 	def initialize(x, y, rotation)
-		@x = x
-		@y = y
+		setPosition(x, y)
 		@rotation = rotation
+		@score = 0
 	end
 
 	def increaseScore
@@ -13,8 +13,8 @@ class Paddle
 	end
 
 	def setPosition(x, y)
-		@x = x;
-		@y = y;
+		@x = x
+		@y = y
 	end
 end
 end
